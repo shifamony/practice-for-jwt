87d7fc6cb6df32e5233fc0cb908b86007c2a4cb8
jhara islam <103064101+jharaislam19@users.noreply.github.com>
2022-11-05 23:02:45 +0600

    first commit

---
+++ src/Pages/Home/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Copy&copy; rights reserved,2022</p>
+        </div>
+    );
+};
+
+export default Footer;
